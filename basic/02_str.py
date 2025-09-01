100ff69432b480675b733e1ae98a9af708089358
jiyoungbb <jiiyoung0906@naver.com>
2025-09-01 21:31:23 +0900

    num, str

---
+++ basic/02_str.py
@@ -0,0 +1,29 @@
+type('hello')
+
+type('111')
+
+type('111+111')
+
+type(' ')
+
+type('')
+
+
+# 문자의 사칙연산
+print("hello"+ " " + "world")
+print("*" * 20)
+
+# 줄바꿈 - "" 3개 사용
+print("""안녕
+      안녕
+      안녕""")
+
+# 줄바꿈 - 이스케이프 문자 사용
+print("hi \n hi \n hi")
+
+
+# "" 출력 - 이스케이프 문자 사용
+print("the \"python\" basic")
+
+# "" 출력 - 큰따옴표("), 작은 따옴표(') 혼합 사용 
+print("the 'python' basic")
