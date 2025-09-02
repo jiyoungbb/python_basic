afe0e7094431bfe865bcb3d8153247debc532df3
jiyoungbb <jiiyoung0906@naver.com>
2025-09-02 19:52:40 +0900

    indexing

---
+++ basic/03_indexing.py
@@ -0,0 +1,20 @@
+# 문자열 갯수
+print(len("hello"))
+print(len("1000"))
+
+# 정수형 len > 에러
+# print(len(100))
+
+# 특정 문자열만 출력
+print("hello"[0])
+print("hello"[4])
+
+# 문자열의 길이를 모르느 경우 > 마지막 글자 출력
+print("hello"[-1])
+
+
+# 슬라이싱
+print("hello"[0:3])
+print("hello"[0:])
+print("hello"[:])
+print("hello"[:-1]) 
