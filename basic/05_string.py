b406d040a7f42ae5d817d59f622dc2c077e6d27a
jiyoungbb <jiiyoung0906@naver.com>
2025-09-04 19:58:37 +0900

    정렬 추가

--- basic/05_string.py
+++ basic/05_string.py
@@ -9,10 +9,15 @@ age = 29
 print("hi my name is " + name + " my job is " + job + " i'm " + str(age))
 
 # 해결방법2 %s
-print("hi my name is %s my job is %s i'm %s" %(name, job, age))
+print("hi my name is %s my job is %s i'm %d" %(name, job, age))
 
 # 해결방법3 format
 print("hi my name is {0} my job is {1} i'm {2}" .format(name, job, age))
 
 # 해결방법4 fstring   << 주로 사용
 print(f"hi my name is {name} my job is {job} i'm {age}")
+
+
+print(f"|{name:<10}|")  # 왼쪽 정렬
+print(f"|{name:>10}|")  # 오른쪽 정렬
+print(f"|{name:^10}|")  # 가운데 정렬
