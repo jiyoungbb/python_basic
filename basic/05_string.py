e3782c5ef20da36bd561ea835f3f703ace8c3739
jiyoungbb <jiiyoung0906@naver.com>
2025-09-04 19:46:54 +0900

    var string

--- basic/05_string.py
+++ basic/05_string.py
@@ -14,5 +14,5 @@ print("hi my name is %s my job is %s i'm %s" %(name, job, age))
 # 해결방법3 format
 print("hi my name is {0} my job is {1} i'm {2}" .format(name, job, age))
 
-# 해결방법4 fprinting   << 주로 사용
+# 해결방법4 fstring   << 주로 사용
 print(f"hi my name is {name} my job is {job} i'm {age}")
