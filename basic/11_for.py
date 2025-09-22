b601d46d1ae557fecce910d1be96ed5c3d641b2b
jiyoungbb <jiiyoung0906@naver.com>
2025-09-22 21:32:25 +0900

    for

---
+++ basic/11_for.py
@@ -0,0 +1,15 @@
+days = ["월", "화", "수", "목", "금"]
+
+for day in days:
+    print(f"{day}요일")
+
+print("주말")
+
+
+
+# for문 범위 지정
+for i in range(1,11):
+    print(f"{i} 바퀴")
+    i += 1
+
+print("종료")
---
+++ basic/12_for_2.py
@@ -0,0 +1,7 @@
+# for문으로 구구단 만들기
+
+for i in range(1,10):
+    for j in range(1,10):
+        print(f"{i} x {j} = {i*j}")
+
+print("종료")
